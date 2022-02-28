ab110e3b6d703c381de58e02df2523199794f616
Hao Zhou <hzhou.me@gmail.com>
2022-02-27 23:06:29 -0800

    use context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { AppContainer } from "./styles";
 import { Column } from "./Column";
 import { AddNewItem } from "./AddNewItem";
+import { useAppState } from "./state/AppStateContext";
 
 export const App = () => {
+  const { lists } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="Todo:" />
+      {lists.map((l) => (
+        <Column text={l.text} id={l.id} key={l.id} />
+      ))}
+
       <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
   );
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,19 +1,28 @@
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
+import { useAppState } from "./state/AppStateContext";
 import { ColumnContainer, ColumnTitle } from "./styles";
 
 type ColumnProps = {
+  id: string;
   text: string;
 };
 
-export const Column = ({ text }: ColumnProps) => {
+export const Column = ({ id, text }: ColumnProps) => {
+  const { getTasksByListId } = useAppState();
+  const tasks = getTasksByListId(id);
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      <Card text="1st Item" />
-      <Card text="2nd Item" />
-      <Card text="3rd Item" />
-      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} dark />
+      {tasks.map((t) => (
+        <Card id={t.id} key={t.id} text={t.text} />
+      ))}
+      <AddNewItem
+        toggleButtonText="+ Add another list"
+        onAdd={console.log}
+        dark
+      />
     </ColumnContainer>
   );
 };
