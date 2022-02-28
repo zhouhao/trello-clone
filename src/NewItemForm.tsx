2e9283d1f5d1f0cb119e7853061939cd8e220c6b
Hao Zhou <hzhou.me@gmail.com>
2022-02-27 22:12:27 -0800

    enable enter key event

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { NewItemButton, NewItemFormContainer, NewItemInput } from "./styles";
 import { useFocus } from "./utils/useFocus";
 
@@ -10,9 +10,20 @@ export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
   const inputRef = useFocus();
 
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onAdd(text);
+    }
+  };
+
   return (
     <NewItemFormContainer>
-      <NewItemInput ref={inputRef} value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onKeyPress={handleAddText}
+        onChange={(e) => setText(e.target.value)}
+      />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
   );
