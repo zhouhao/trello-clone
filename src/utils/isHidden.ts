942b9d6624c88ef9500e5c73d56f4624e55d6f66
Hao Zhou <hzhou.me@gmail.com>
2022-03-05 20:40:54 -0800

    semitransparent the column when dargging

---
+++ src/utils/isHidden.ts
@@ -0,0 +1,10 @@
+import { DragItem } from "./../DragItem";
+export const isHidden = (
+  draggedItem: DragItem | null,
+  itemType: string,
+  id: string
+): boolean => {
+  return Boolean(
+    draggedItem && draggedItem.type === itemType && draggedItem.id === id
+  );
+};
